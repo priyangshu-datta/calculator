ac3e81fbb77a259af479b3b6e36ebcc92bea90c6
Priyangshu Datta <dattapapun2468@gmail.com>
2022-06-17 15:50:44 +0530

    using <Show/> instead of ternary operator

--- src/modal/index.tsx
+++ src/modal/index.tsx
@@ -1,4 +1,4 @@
-import { Component } from "solid-js";
+import { Component, Show } from "solid-js";
 import { setState } from "../store/modal";
 import { history } from "../store/history";
 import { setExpression } from "../store/current";
@@ -16,8 +16,15 @@ const index: Component = () => {
                 class="shadow-md shadow-black min-w-[350px] max-w-[40vw] relative bg-black text-white text-center h-fit pt-8 pb-6 px-4 max-h-[70vh] rounded-md cursor-default"
             >
                 <div class="overflow-y-auto grid gap-2 px-2">
-                    {history().length > 0 ? (
-                        history().map((h) => {
+                    <Show
+                        when={history().length > 0}
+                        fallback={
+                            <span class="text-gray-400 select-none">
+                                Nothing yet
+                            </span>
+                        }
+                    >
+                        {history().map((h) => {
                             return (
                                 <div
                                     class="grid grid-cols-3 cursor-pointer"
@@ -38,12 +45,8 @@ const index: Component = () => {
                                     </div>
                                 </div>
                             );
-                        })
-                    ) : (
-                        <span class="text-gray-400 select-none">
-                            Nothing yet
-                        </span>
-                    )}
+                        })}
+                    </Show>
                 </div>
                 <button
                     class="bg-[rgb(72,83,90)] px-4 py-2 mt-4 rounded-lg font-semibold select-none"
